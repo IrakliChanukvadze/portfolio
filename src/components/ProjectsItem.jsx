912516a4c89873d4d397e64bd3b5a336b9a7e822
irakli tchanuyvadze <iraklichanuyvadze@gmail.com>
2023-02-04 15:25:45 +0400

    modified positioning of github and demo links inside project

--- src/components/ProjectsItem.jsx
+++ src/components/ProjectsItem.jsx
@@ -5,11 +5,11 @@ const ProjectsItem = ({ image, title, link, demoLink }) => {
   return (
     <div className="relative flex items-start justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#fab466] to-[#001b5e] ">
       <img src={image} className=" rounded-xl group-hover:opacity-10" />
-      <div className="hidden text-white text-bold group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
+      <div className="hidden text-white text-bold group-hover:flex absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex-col items-center ">
         <h3 className="text-2xl text-center text-white font-bold uppercase tracking-[1.5px] ">
           {title}
         </h3>
-        <div className="flex gap-24 pt-2">
+        <div className="flex pt-2 w-40 justify-between">
           <a href={demoLink} target="_blank">
             Demo{" "}
           </a>
